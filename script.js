9a5f4b120fc1c4e445d71d717d1a0bcdbd16e058
Cade Murphy <cademurphy9@gmail.com>
2021-12-15 17:08:31 +1000

    The first function was implemented

---
+++ script.js
@@ -0,0 +1,13 @@
+let sentence = ""
+
+
+const sentenceChoice = (noun, verb, adjective) => {
+	let rNum = Math.floor(Math.random() * 3);
+
+	if (rNum === 1) {
+
+		return sentence = `The ${noun} was ${adjective} ${verb} while the ${noun}'s best friend watched`
+
+	}
+
+}
