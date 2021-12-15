4a8c38a640fda3de94a34cfdc13985d2f74d5c59
Cade Murphy <cademurphy9@gmail.com>
2021-12-15 17:19:33 +1000

    Final Draft

--- script.js
+++ script.js
@@ -9,5 +9,19 @@ const sentenceChoice = (noun, verb, adjective) => {
 		return sentence = `The ${noun} was ${adjective} ${verb} while the ${noun}'s best friend watched`
 
 	}
+	else if (rNum === 2) {
 
+		return sentence = `${verb} with the ${noun}, they both ${adjective} changed the tide of the battle`
+
+
+
+	} else if (rNum === 3) {
+
+		return sentence = `The young ${noun} was ${verb} in the leaves waiting for the ${adjective} wind to come`
+
+
+	}
 }
+
+
+sentenceChoice("Keyboard", "stabbing", "Brutally");
